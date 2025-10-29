c186e1d990b7b6a599406aff5f14f0b9fdfa34f1
afzalhossainraju100 <raju23105341031@diu.edu.bd>
2025-10-29 15:00:49 +0600

    set, get , display to console, remove from local storage easy statement

---
+++ commonLocalStorage.js
@@ -0,0 +1,13 @@
+//set item to local storage
+localStorage.setItem("user", "Afzal Hossain");
+localStorage.setItem("user", "Afzal");
+localStorage.setItem("name", "Hossain");
+//get item from local storage
+const username= localStorage.getItem("username");
+const user= localStorage.getItem("user");
+const name= localStorage.getItem("name");
+//displaying the values
+console.log(username);
+console.log(user);
+console.log(name);
+
