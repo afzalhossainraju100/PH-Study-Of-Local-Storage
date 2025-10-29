fc43315a7b61a639036e4fb152664875e9c59da6
afzalhossainraju100 <raju23105341031@diu.edu.bd>
2025-10-29 15:16:20 +0600

    remove, clear, check length, and keys of localstorage

--- commonLocalStorage.js
+++ commonLocalStorage.js
@@ -1,5 +1,5 @@
 //set item to local storage
-localStorage.setItem("user", "Afzal Hossain");
+localStorage.setItem("username", "Afzal Hossain");
 localStorage.setItem("user", "Afzal");
 localStorage.setItem("name", "Hossain");
 //get item from local storage
@@ -10,4 +10,18 @@ const name= localStorage.getItem("name");
 console.log(username);
 console.log(user);
 console.log(name);
-
+//remove item from local storage
+localStorage.removeItem("name");
+//clear all the local storage
+localStorage.clear();
+//set item to local storage
+localStorage.setItem("username", "Afzal Hossain");
+localStorage.setItem("user", "Afzal");
+localStorage.setItem("name", "Hossain");
+//checking the keys length
+console.log(localStorage.length);
+//getting key by index
+const key0= localStorage.key(0);
+const key1= localStorage.key(1);
+console.log(key0);
+console.log(key1);
